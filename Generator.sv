d2e7625a2d93f6c1868855a1f2f5790e0bdf48b8
KeyuriS0809 <keyuri.sorathiya@asicraft-tech.com>
2026-05-19 12:18:06 +0530

    Update Generator.sv

--- Generator.sv
+++ Generator.sv
@@ -2,21 +2,14 @@ class generator;
 
   mailbox #(transcation) gen2drv;
   transcation trans;
-  event drv_done;
 
-  function new(mailbox #(transcation) gen2drv, event drv_done);
+  function new(mailbox #(transcation) gen2drv);
     this.gen2drv  = gen2drv;
-    this.drv_done = drv_done;
   endfunction
-
-  task main();
-    trans = new();
-  endtask
 
   task send(transcation tr);
     tr.display("GENERATOR");
     gen2drv.put(tr);
-    @(drv_done);
   endtask
 
 endclass
